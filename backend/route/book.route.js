e4179efa0764101393b059be5f28f372bab263ef
Suraj Yadav <ersuraj097@gmail.com>
2025-02-02 22:02:15 +0530

    Delete backend directory

--- backend/route/book.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getBook } from "../../../BookStore/backend/controller/book.controller.js";
-
-const router = express.Router();
-
-router.get("/", getBook);
-
-export default router;
