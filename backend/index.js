e8ef2a2dfce428b53552bf5c70b29aa7e17cc3aa
Suraj Yadav <ersuraj097@gmail.com>
2025-02-02 23:37:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -43,13 +43,7 @@ app.use("/user", userRoute);
 
 //deployment
 
-if (process.env.NODE_ENV === "production"){
-    const dirPath = path.resolve();
-    app.use(express.static("Frontend/dist"))
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(dirPath, "Frontend", "dist", "index.html"));
-    })
-}
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
